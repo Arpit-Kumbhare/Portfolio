14ee58ef78884e1479e27d2347758b58a589ca10
Arpit Kumbhare <arpitkumbhare007@gmail.com>
2025-06-02 01:32:27 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Navbar from './components/navbar'
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+      <p className='font-mono text-red-700 text-lg font-bold'>hello</p>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const navbar = () => {
+  return (
+    <div>
+      <nav className='bg-blue-400 text-center text-2xl text-red-400'> 
+        this is navbar
+      </nav>
+    </div>
+  )
+}
+
+export default navbar

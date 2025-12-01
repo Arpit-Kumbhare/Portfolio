76fcf5ebffe57f3469e53b8c4ab80898a9099c36
Arpit Kumbhare <arpitkumbhare007@gmail.com>
2025-12-01 21:58:28 +0530

    fix: deploy mode-aware added in vite.config & workflow

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  base: "/",
-  server: { 
-    open: true,
-    port: 30001, 
+export default defineConfig(({mode}) => {
+  const isGithub = mode === 'github';
+
+  return {
+    plugins: [react()],
+    base: isGithub ? '/Portfolio' : '/',
+    server: {
+      open: true,
+      port: 30001,
     },
+  };
 });
